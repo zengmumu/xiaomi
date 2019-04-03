083868f74b78e0ca00956403f74939860e758eaf
mumu <zengmumu@126.com>
2019-04-03 23:11:12 +0800

    2.11

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home content has-tabs">
+    <div class="bar-header row">
+        <div class="col col-20 tac">logo</div>
+        <div class="col">
+          <input type="text">
+        </div>
+        <div class="col col-20 tac">user</div>
+    </div>
+    <div class="sub-header row"> sub header</div>
+    <div class="content has-header has-sub">
+      <div class="swiper">
+        <img src="./../assets/images/slide1.webp" width="100%" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+   
+  }
+}
+</script>
